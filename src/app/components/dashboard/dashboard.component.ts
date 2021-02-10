fdcfe47e581fb9f144a295084543307a1b4ded55
vikram-sapate123 <vikram.sapate123@gmail.com>
2021-02-10 16:52:07 +0530

    added interfaces

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -5,6 +5,8 @@ import { DataService } from 'src/app/services/data.service';
 import { ComposeComponent } from '../compose/compose.component';
 import { ReadComponent } from '../read/read.component';
 import { SentComponent } from '../sent/sent.component';
+import { Mail } from './mail.interface';
+import { User } from './user.interface';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,25 +14,25 @@ import { SentComponent } from '../sent/sent.component';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  mails: any;
-  to: any;
-  cc: any;
-  sub: any;
-  mail: any;
-  allMails: any;
+  mails: Mail[] = [];
+  allMails: Mail[] = [];
   isMenuExpanded = true;
   unReadMails = 0;
-  constructor(private router: Router, public dialog: MatDialog, private dataService: DataService) {}
+  constructor(
+    private router: Router,
+    public dialog: MatDialog,
+    private dataService: DataService
+  ) {}
 
   ngOnInit(): void {
     this.allMails = JSON.parse(localStorage.getItem('mails')!);
-    this.mails = this.allMails.filter((mail: any) => {
+    this.mails = this.allMails.filter((mail: Mail) => {
       return (
         mail.to == this.dataService.userEmail ||
         mail.cc == this.dataService.userEmail
       );
     });
-    this.unReadMails = this.mails.filter((m: any) => {
+    this.unReadMails = this.mails.filter((m: Mail) => {
       return m.isRead == false;
     }).length;
   }
@@ -46,17 +48,17 @@ export class DashboardComponent implements OnInit {
   searchEmail($event: any) {
     $event.target.blur();
   }
-  deleteMail(mail: any) {
+  deleteMail(mail: Mail) {
     if (mail.isRead == false) {
       this.unReadMails--;
     }
-    this.mails = this.mails.filter((m: any) => {
+    this.mails = this.mails.filter((m: Mail) => {
       return !(m.from == mail.from && mail.time == m.time);
     });
   }
   onSentMail() {
     this.allMails = JSON.parse(localStorage.getItem('mails')!);
-    const sentMails = this.allMails.filter((m: any) => {
+    const sentMails = this.allMails.filter((m: Mail) => {
       return m.from == this.dataService.userEmail;
     });
     const dialogRef = this.dialog.open(SentComponent, {
@@ -76,7 +78,7 @@ export class DashboardComponent implements OnInit {
       if (!data) return;
       if (!data.to) return;
       const accounts = JSON.parse(localStorage.getItem('accounts')!);
-      const account = accounts.find((acc: any) => {
+      const account = accounts.find((acc: User) => {
         return acc.email == this.dataService.userEmail;
       });
       const name = account.name;
@@ -96,8 +98,8 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  openMail(openedMail: any) {
-    const mail = this.mails.find((m: any) => {
+  openMail(openedMail: Mail) {
+    const mail: any = this.mails.find((m: Mail) => {
       return m.from == openedMail.from && openedMail.time == m.time;
     });
     if (!mail.isRead) {
---
+++ src/app/components/dashboard/mail.interface.ts
@@ -0,0 +1,10 @@
+export interface Mail {
+  senderName: string;
+  from: string;
+  to: string;
+  cc: string;
+  sub: string;
+  time: string;
+  mail: string;
+  isRead: boolean;
+}
